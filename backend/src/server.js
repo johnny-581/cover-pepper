99ab46da1a9a5e7b9b59f872da30f8f788b021c2
Jiayang Dong <dongjiayang123@hotmail.com>
2025-09-01 12:29:47 -0700

    fixed cookie.secure

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.use(
         cookie: {
             httpOnly: true,
             sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
-            secure: false,
+            secure: ENV.NODE_ENV === "production",
             maxAge: 1000 * 60 * 60 * 24 * 7,
         },
         store: new PrismaSessionStore(prisma, {
