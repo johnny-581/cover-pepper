a6798b1b086853cd0a7ca121b1a0102105317388
Jiayang Dong <dongjiayang123@hotmail.com>
2025-09-02 19:39:47 -0700

    testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,6 +34,7 @@ app.use(
             httpOnly: true,
             sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
             secure: ENV.NODE_ENV === "production",
+            partitioned: true,
             maxAge: 1000 * 60 * 60 * 24 * 7,
         },
         store: new PrismaSessionStore(prisma, {
